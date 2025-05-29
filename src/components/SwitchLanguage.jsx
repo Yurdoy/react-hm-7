55d1f3f5776b0481173215a64a8120ae9bf0d9f1
Yurdoy <yurdoy@gmail.com>
2025-05-29 19:53:53 +0200

    context file and component created

---
+++ src/components/SwitchLanguage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SwitchLanguage = () => {
+  return (
+    <div>
+      <h2></h2>
+      <button></button>
+    </div>
+  );
+};
+
+export default SwitchLanguage;
---
+++ src/context/currentLanguage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const currentLanguage = React.createContext("English");
+
+export default currentLanguage;
